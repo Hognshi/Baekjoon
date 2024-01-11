c37efb1bed9e99846858d96df0ede16ff12e1ae7
Hongshi <123186080+Hognshi@users.noreply.github.com>
2024-01-11 15:16:54 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++)
+    {
+        sum += i+1;
+    }
+    cout << sum;
+    
+}
