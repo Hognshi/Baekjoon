2cc3b90fb39e3dbed093f2aa96dc29b9eb3bca9b
Hongshi <123186080+Hognshi@users.noreply.github.com>
2024-01-13 14:58:26 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int t;
+	string s;
+
+	cin >> t >> s;
+	
+	int sum = 0;
+
+	for (int i = 0; i < t; i++) {
+		sum += s[i] - '0';
+	}
+
+	cout << sum;
+
+
+	return 0;
+}
