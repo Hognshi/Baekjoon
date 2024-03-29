6831c49f8096150e5e6c6ddd5278cd9d3936d6f5
Hognshi <123186080+Hognshi@users.noreply.github.com>
2024-03-29 09:51:07 +0900

    [Gold IV] Title: 전화번호 목록, Time: 72 ms, Memory: 3176 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> v;
+        for (int i = 0; i < N; i++)
+        {
+            string str;
+            cin >> str;
+            v.push_back(str);
+        }
+        sort(v.begin(), v.end());
+        bool coherent = true;
+        for (int i = 0; i < v.size() - 1; i++)
+        {
+            if (v[i] == v[i + 1].substr(0, v[i].length())) {
+                coherent = false;
+                break;
+            }
+        }
+        // 일관성이 있으면
+        if (coherent) {
+            cout << "YES" << '\n';
+        }
+        else {
+            cout << "NO" << '\n';
+        }
+    }
+    return 0;
+}
